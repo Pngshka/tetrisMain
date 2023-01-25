cb46446a30eaff6d10818d58e2cc1e2af223e4b7
v.zhigalov <v.zhigalov@peppers-studio.ru>
2023-01-25 11:06:36 +0300

    refactor: переименованы классы в примерах

--- utils/scene/examples/controller/CustomThreeWrapper.js
+++ utils/scene/examples/controller/CustomThreeWrapper.js
@@ -1,12 +1,12 @@
 import ThreeWrapper from "../../wrappers/threejs/ThreeWrapper";
 import Data from "../data/Data";
 
-export default class ThreeController extends ThreeWrapper {
+export default class CustomThreeWrapper extends ThreeWrapper {
 
   storage = new Data();
 
   static get instance() {
-    if (!this._instance) this._instance = new ThreeController();
+    if (!this._instance) this._instance = new CustomThreeWrapper();
 
     return this._instance;
   }
