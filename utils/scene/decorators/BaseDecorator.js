b76301bb2ea11059f1ab70ffebc03b283a0cda36
v.zhigalov <v.zhigalov@peppers-studio.ru>
2023-05-30 13:27:36 +0300

    chore: обновлен шаблон

--- utils/scene/decorators/BaseDecorator.js
+++ utils/scene/decorators/BaseDecorator.js
@@ -3,7 +3,7 @@ export default class BaseDecorator {
   define(target, property, get, set) {
     const attributes = {get, set};
 
-    if (Object.hasOwn(target, property)) return;
+    if (target.hasOwnProperty(property) && target[property] === undefined) return;
     Object.defineProperty(target, property, attributes)
   }
 }
